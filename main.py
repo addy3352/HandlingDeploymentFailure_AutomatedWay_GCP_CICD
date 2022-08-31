5c54bb05b07d83eff10fe96f722434ec5808e642
Aditya Raman <aditya.raman@gmail.com>
2022-08-31 15:17:02 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,4 +1,3 @@
-
 from concurrent.futures import process
 from distutils.command.build import build
 from doctest import OutputChecker
@@ -24,26 +23,23 @@ app = Flask(__name__)
 @app.route('/',methods=['post'])
 def index():
     envelope = request.get_json()
-    print("envelope is {} and type is {}".format(envelope,type(envelope)))
     pubsub_message = envelope["message"]
-    print("message is {} and type is {}".format(pubsub_message,type(pubsub_message)))
-#        error_message=json.loads(envelope)
     if isinstance(pubsub_message, dict):
-        build_id = pubsub_message["attributes"]["buildId"]
+        build_id = pubsub_message["attributes"]["buildId"] ## extract build id from the message
         filter_ID ="ID="+build_id
         format_trigger = 'value(substitutions.TRIGGER_NAME)'
         try:
-            outpu1=subprocess.check_output(["gcloud", "builds", "list", "--filter",filter_ID,"--format",format_trigger])
+            outpu1=subprocess.check_output(["gcloud", "builds", "list", "--filter",filter_ID,"--format",format_trigger]) ## get trigger name from build id
         except:
             print("error in retrieving trigger id")
         else:
             output1=outpu1.decode("utf-8")
+            output1 = output1.strip("\n") ##stripped \n
             trigger_name = output1  ###
-            print("trigger name is {}".format(trigger_name))
-            filter_trigger = "status=SUCCESS AND substitutions.TRIGGER_NAME="+trigger_name 
+            filter_trigger = "status=SUCCESS AND substitutions.TRIGGER_NAME="+trigger_name  ### Only Successful build needs to be filtered
             format_commit = "value(substitutions.COMMIT_SHA)"
             try:
-                output2=subprocess.check_output(["gcloud", "builds", "list", "--filter",filter_trigger,"--format",format_commit])
+                output2=subprocess.check_output(["gcloud", "builds", "list", "--filter",filter_trigger,"--format",format_commit]) ## Get Commit id from trigger name
             except:
                 print("error in retriving commit_id")
             else:
@@ -51,27 +47,48 @@ def index():
                 commit_list = output2.split('\n')
                 commit_id = commit_list[0]
                 print("latest commit_id is {}".format(commit_id))
-            if trigger_name == "cloud-mad-terraform":    
-                path='/app/cloud-mad-terraform'
-                os.chdir(path)
-                try:
-                    output3=subprocess.check_output(["git", "checkout",commit_id,"."])
-                except:
-                    print("Check out error with commit id {}".format(commit_id))
-                    SUCCESS =24
-                else:
-                    message="Reverting to " + commit_id
+            if trigger_name == "<trigger_name>":
+                path='./<repo_name>'
+                print("Checked in")
+                if os.path.exists(path):
                     try:
-                        output4=subprocess.check_output(["git", "add","."])
-                        output5=subprocess.check_output(["git", "commit","-m",message])
-                        output6=subprocess.check_output(["git", "push"])
+                        output=subprocess.check_output(["rm","-r",path])   ### delete existing directory
                     except:
-                        print("errorafter check out in pushing ")
-                        SUCCESS =25
+                        print("error in deleting directory")
                     else:
-                        SUCCESS =0
+                        print("directory deleted successfully")
+                else:
+                    print("path doesn't exists")    
+                try:
+                    output11=subprocess.check_output(["git","init"])   
+                    output7= subprocess.check_output(["git","clone","-b","<branch_name>","https://<personal access token>@github.com/<user_id>/<repo_name>.git"]) 
+                    output8=subprocess.check_output(["git","remote","add" ,"base","https://<personal access token>@github.com/addy3352/<repo_name>.git"])
+                    output9=subprocess.check_output(["git","config","--global" ,"user.email","<user email id>"])
+                    output10=subprocess.check_output(["git","config","--local" ,"-l"])   
+                except:
+                    print(" Error in cloning")
+                else:
+                    os.chdir(path)
+                    try:
+                        output3=subprocess.check_output(["git","checkout",commit_id,"."])
+                    except:
+                        print("Check out error with commit id {}".format(commit_id))
+                        SUCCESS =24
+                    else:
+                        message="Reverting to " + commit_id
+                        output4=subprocess.check_output(["git", "add","."])
+                        try:
+#                            output4=subprocess.check_output(["git", "add","."])
+                            output5=subprocess.check_output(["git", "commit","-m",message])
+                            output6=subprocess.check_output(["git", "push"])
+                        except:
+                            print("errorafter check out in pushing-")
+                            SUCCESS =25
+                        else:
+                            SUCCESS =0
+            else:
+                print("trigger name is not present {}".format(trigger_name))
         return(",204")
     else:
         print("bad message format")
-        return(",100")
-#    return render_template('alerts&notification.html',message)
+        return(",400")
